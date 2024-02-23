53aab0419d79472ace691286f4cfd141a78c8c10
Tomislav Tipurić <tomislavt@hotmail.com>
2024-02-23 15:31:18 +0100

    Added API communication example and edited readme.

--- README.md
+++ README.md
@@ -1,15 +1,91 @@
-This is an example Rust project used in Daytona.
+# Code100 Competition - Rust Sample
 
-[Try it out](https://daytona.io/#https://github.com/daytonaio-templates/rust)
+This codebase contains everything you need to complete coding challenges of the Code100 competition in Rust.
+The code example of communicating with the API is already present in the `main.rs` file in `src` folder.
+
+If you need additional information regarding the API you may find it in the rest of this document.
+
+### Running the project
 
 Open the terminal and run:
 ```sh
-cd test-project
-rustc hello.rs
-./hello
+cargo run
 ```
-Or just press the *Run Code* button found in the top right of the editor panel.
 
-### Want to contribute?
+## API Base URL
+```
+https://challenger.code100.dev
+```
 
-Feel free to [open a PR](https://github.com/daytonaio-templates/rust) with any suggestions for this test project 😃 
+## Swagger Documentation
+```
+https://challenger.code100.dev/doc/
+```
+## Login
+
+In order to request the coding puzzle or submit the solution, you first need to retrieve the JSON Web Token by logging into the API:
+
+### Request
+```
+POST /login
+```
+### Body
+```
+{
+  "email": <email here>,
+  "password": <password here>
+}
+```
+### Response
+```
+{
+   ...
+   "token": <JWT token>
+   ...
+}
+```
+The token given in this response needs to be supplied for the Request Puzzle and Submit Solution requests.
+
+## Request Puzzle
+
+### Request
+```
+GET /getpuzzle
+```
+### Headers
+```
+Authorization: Bearer <JWT token>
+```
+### Response
+```
+{
+  "name": <name of the puzzle>,
+  "input": <coding challenge>
+}
+```
+(coding challenge depends from round to round)
+
+## Submit Solution
+
+### Request
+```
+POST /postanswer
+```
+### Headers
+```
+Authorization: Bearer <JWT token>
+```
+### Body
+```
+{
+  "answer": <your solution to the posted puzzle>
+}
+```
+### Response
+
+There is a 15 seconds delay for you to receive the answer for this request.
+```
+{
+  "message": <information if solution was correct or not>,
+}
+```
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,74 @@
-extern crate ferris_says;
+use reqwest;
+// use serde::Deserialize;
+// use reqwest::Error;
+use serde_json::json;
 
-use std::io::{ stdout, BufWriter };
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
 
-fn main() {
-    let out: &str = "Hello fellow Rustaceans!";
+    let email = "EMAIL-OR-USERNAME-HERE"; // replace with email or username given to you
+    let password = "PASSWORD-HERE"; // replace with password given to you
 
-    let mut writer: BufWriter<std::io::Stdout> = BufWriter::new(stdout());
-    ferris_says::say(out, out.len(), &mut writer).unwrap();
+    // Step 1: Login
+    let login_data = json!({
+        "email": email,
+        "password": password
+    });
+
+    let login_response = reqwest::Client::new()
+        .post("https://challenger.code100.dev/login")
+        .json(&login_data)
+        .send()
+        .await?;
+
+    if !login_response.status().is_success() {
+        println!("Login failed");
+        return Ok(());
+    }
+
+    let login_result = login_response.json::<serde_json::Value>().await?;
+    let token = login_result["token"].as_str().unwrap();
+
+    // Step 2: Call Authenticated Endpoint
+    let auth_response = reqwest::Client::new()
+        .get("https://challenger.code100.dev/testauthroute")
+        .header("Authorization", format!("Bearer {}", token))
+        .send()
+        .await?;
+
+    let auth_result = auth_response.text().await?;
+    println!("{}", auth_result);
+
+    // Step 3: Get the puzzle
+    let puzzle_response = reqwest::Client::new()
+        .get("https://challenger.code100.dev/getpuzzle")
+        .header("Authorization", format!("Bearer {}", token))
+        .send()
+        .await?;
+
+    let puzzle_result = puzzle_response.text().await?;
+    println!("{}", puzzle_result);
+
+    // Step 4: Solve the puzzle
+
+    ////////////////////////////
+    ////// YOUR CODE HERE //////
+    ////////////////////////////
+
+    let answer = json!({
+        "answer": "some answer in required format"
+    });
+
+    // Step 5: Submit the solution
+    let submit_response = reqwest::Client::new()
+        .post("https://challenger.code100.dev/postanswer")
+        .header("Authorization", format!("Bearer {}", token))
+        .json(&answer)
+        .send()
+        .await?;
+    
+    let submit_result = submit_response.text().await?;
+    println!("{}", submit_result);
+
+    Ok(())
 }
